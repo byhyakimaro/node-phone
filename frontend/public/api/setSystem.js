c8046ee3683b17445fbdf99066dbb60870f22856
byhyakimaro <moderrevolution090217@gmail.com>
2021-10-29 13:41:03 -0300

    all

--- frontend/public/api/setSystem.js
+++ frontend/public/api/setSystem.js
@@ -19,27 +19,83 @@ const appsSystem = [
 {
   name: 'home', 
   html: `
-    <div class="apps">
-      <div class="time">
-        <div class="hora">12</div>
-        <div class="minuto">00</div>
-        <div class="data">qua,17 marco</div>
+    <div class="home-list">
+      <div class="home-item">
+        <div class="time-width">
+          <div class="time">
+            <div class="hora">12</div>
+            <div class="minuto">00</div>
+            <div class="data">qua,17 marco</div>
+          </div>
+        </div>
+        <div class="icons">
+          <div class="icon icon-phone">
+            <img data-app="message" src="https://cdn.discordapp.com/attachments/758616655382577152/834931791109554206/messaging.png">
+          </div>
+          <div class="icon icon-contacts">
+            <img data-app="contatos" src="https://cdn.discordapp.com/attachments/758616655382577152/833170257392173076/contacts.png">
+          </div>
+          <div class="icon icon-whatsapp">
+            <img data-app="whatsapp" src="https://cdn.discordapp.com/attachments/758616655382577152/833170261695004682/whatsapp.png">
+          </div>
+          <div class="icon icon-settings">
+            <img src="https://cdn.discordapp.com/attachments/758616655382577152/833170248248983563/settings.png">
+          </div>
+        </div>
       </div>
+      <div class="home-item"></div>
+      <div class="home-item"></div>
     </div>
-    <div class="icons">
-      <div class="icon icon-phone">
-        <img data-app="message" src="https://cdn.discordapp.com/attachments/758616655382577152/834931791109554206/messaging.png">
-      </div>
-      <div class="icon icon-contacts">
-        <img data-app="contatos" src="https://cdn.discordapp.com/attachments/758616655382577152/833170257392173076/contacts.png">
-      </div>
-      <div class="icon icon-whatsapp">
-        <img data-app="whatsapp" src="https://cdn.discordapp.com/attachments/758616655382577152/833170261695004682/whatsapp.png">
-      </div>
-      <div class="icon icon-settings">
-        <img src="https://cdn.discordapp.com/attachments/758616655382577152/833170248248983563/settings.png">
-      </div>
-    </div>`
+    `,
+    style: `
+    .home {
+      padding: 3rem 0.65rem 4rem 0.65rem;
+    }
+    .home-list,.home-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .home-list {
+      height: 85%;
+      flex-direction: row;
+      transition: transform .1s;
+    }
+    .home-item {
+      min-width: 100%;
+      height: 100%;
+    }
+    .time-width {
+      padding: 1rem;
+    }
+    .time-width .time {
+      text-shadow: 0.1em 0.1em 0.2em black;
+      font-size: 70px;
+    }
+    .time-width .hora, .time-width .minuto {
+      padding-left: 10px;
+    }
+    .time-width .time .data {
+      font-size: 13px;
+    }
+    .icons {
+      cursor: pointer;
+      display: flex;
+      flex-wrap: wrap;
+      width: 90%;
+      justify-content: center;
+      position: absolute;
+      bottom: 80px;
+    }
+    .icons .icon {
+      width: 43px;
+      height: 43px;
+      padding: 5px 10px;
+    }
+    .icons .icon:hover {
+      transform: scale(1.05);
+    }
+    `,
 }
 ]
 
